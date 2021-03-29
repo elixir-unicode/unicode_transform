5a93ee70ff0eebb8645e0bd028d675953c4b9c20
Kip Cole <kipcole9@gmail.com>
2021-03-29 08:28:23 +0800

    Ready package for initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog for Unicode Transform 0.1.0
+
+This is the changelog for Unicode Transform 0.1.0 released on March 29th, 2021.  For older changelogs please consult the release tag on [GitHub](https://github.com/elixir-unicode/unicode_set/tags)
+
+## Enhancements
+
+* Initial release. Supports the transform of Latin-1 to ASCII. See `Unicode.Transform.LatinAscii.transform/1`.
--- mix.exs
+++ mix.exs
@@ -1,13 +1,50 @@
 defmodule UnicodeTransform.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :unicode_transform,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs(),
+      name: "Unicode Transform",
+      source_url: "https://github.com/elixir-unicode/unicode_transform",
+      description: description(),
+      package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      dialyzer: [
+        plt_add_apps: ~w(mix)a,
+        ignore_warnings: ".dialyzer_ignore_warnings"
+      ]
+    ]
+  end
+
+  defp description do
+    """
+    Implementation of the Cldr Transform specification to
+    transform text from one script to another.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Kip Cole"],
+      licenses: ["Apache 2.0"],
+      logo: "logo.png",
+      links: links(),
+      files: [
+        "lib",
+        "logo.png",
+        "mix.exs",
+        "README*",
+        "CHANGELOG*",
+        "LICENSE*"
+      ]
     ]
   end
 
@@ -23,4 +60,31 @@ defmodule UnicodeTransform.MixProject do
       {:sweet_xml, "~> 0.6", only: [:dev, :test], optional: true}
     ]
   end
+
+  def links do
+    %{
+      "GitHub" => "https://github.com/elixir-unicode/unicode_transform",
+      "Readme" => "https://github.com/elixir-unicode/unicode_transform/blob/v#{@version}/README.md",
+      "Changelog" =>
+        "https://github.com/elixir-unicode/unicode_transform/blob/v#{@version}/CHANGELOG.md"
+    }
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      logo: "logo.png",
+      extras: [
+        "README.md",
+        "LICENSE.md",
+        "CHANGELOG.md"
+      ],
+      skip_undefined_reference_warnings_on: ["changelog", "CHANGELOG.md"]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test"]
+  defp elixirc_paths(:dev), do: ["lib", "bench"]
+  defp elixirc_paths(_), do: ["lib"]
 end
