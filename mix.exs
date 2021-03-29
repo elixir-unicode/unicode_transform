27cb69375d5d5b2af73cc2316aa78a1c2a0ff398
Kip Cole <kipcole9@gmail.com>
2021-03-29 12:03:30 +0800

    unicode_set is required, not optional for now

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule UnicodeTransform.MixProject do
 
   defp deps do
     [
-      {:unicode_set, "~> 0.12", optional: true},
+      {:unicode_set, "~> 0.12"},
       {:sweet_xml, "~> 0.6", only: [:dev, :test], optional: true},
       {:ex_doc, "~> 0.19", runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false, optional: true}
