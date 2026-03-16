49462342f4d6fd0cf4fd1a10eb6b29d74ebbaedc
Kip Cole <kipcole9@gmail.com>
2026-03-16 17:31:06 +1100

    Remove .o from the repo

--- c_src/env.mk
+++
@@ -1,4 +0,0 @@
-ERTS_INCLUDE_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/erts-16.2/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/lib/erl_interface-5.6.2/include
-ERL_INTERFACE_LIB_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/lib/erl_interface-5.6.2/lib
-
